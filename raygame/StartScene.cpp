560a1847130e939ac971ccb42050bb29f1ee57b6
Michael Lofton <michaellofton32@gmail.com>
2022-01-05 10:37:23 -0600

    Spawner class exists

--- raygame/StartScene.cpp
+++ raygame/StartScene.cpp
@@ -4,6 +4,7 @@
 #include "Sprite.h"
 #include "MovementComponent.h"
 #include "Transform2D.h"
+#include "EnemySpawner.h"
 
 void StartScene::start()
 {
@@ -13,6 +14,10 @@ void StartScene::start()
 	Enemy* enemy = new Enemy(55, 50, "Enemy", player);
 	enemy->getTransform()->setScale({ 50, 50 });
 
+	/*EnemySpawner* spawner1 = new EnemySpawner(100, 5, "Spawner1", enemy);*/
+
+
 	addActor(player);
 	addActor(enemy);
+	/*addActor(spawner1);*/
 }
